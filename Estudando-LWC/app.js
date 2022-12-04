6598a130ba1142bb11fc3fa96b2615e3c52d287d
Guilherme Garpelli <ggarpelli@gmail.com>
2022-12-04 12:32:42 -0300

    Adicionando Arquivos

---
+++ Estudando-LWC/app.js
@@ -0,0 +1,22 @@
+//console.log("Bem Vindo ao Log");
+
+//console.error("Eu sou um erro");
+
+//console.warn("Aviso");
+
+//comentando do JS
+
+/* é igual no apex
+
+abre lá, e fecha aqui
+
+*/
+
+//console.log("Aqui NÃO ESTÁ COMENTANDO MAIS");
+
+//Add Datatypes 04/12/2022
+const sobreNome = 'Parise Garpelli'
+const numeroCPF = 42209887852
+const brasileiro = true
+console.log(sobreNome,':', numeroCPF,':', brasileiro);
+console.log(typeof sobreNome, typeof numeroCPF, typeof brasileiro);
